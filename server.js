3a64330aa1d0801883b22516852648cd0363faa1
Joshua Philips <jmichaelphilips@gmail.com>
2016-09-23 11:30:16 -0500

    added my first route

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 'use strict'
 
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.set('port', port)
+
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome home</h1>')
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+})
