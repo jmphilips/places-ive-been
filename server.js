b03e64f3b15d15ca422f19a19f554df6d0f7457a
Joshua Philips <jmichaelphilips@gmail.com>
2016-09-23 20:33:46 -0500

    pug rendered index

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.set('port', port)
+app.set('port', port);
+
+// Middlewares 
+app.use(express.static('./public'));
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.send('<h1>Welcome home</h1>')
+    res.render('home')
 });
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`)
-})
+});
